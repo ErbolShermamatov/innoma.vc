0b1237ac49f8215ece850f00cbcd48954bd4aaf1
Erbol <erbol.shermamatov7@gmail.com>
2026-02-17 18:34:34 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+const burgerButton = document.querySelector('.burger-button');
+const menu = document.getElementById('menu')
+burgerButton.addEventListener('click', () => {
+    menu.showModal();
+})
+
+const allTabsButtons = document.querySelectorAll('.tabs__button');
+const allTabs = document.querySelectorAll('.tabs__content-item');
+function removeIsActiveClassButtons() {
+    allTabsButtons.forEach(button => {
+        button.classList.remove('is-active');
+    })
+}
+function removeIsActiveClassTabs() {
+    allTabs.forEach(tab => {
+        tab.classList.remove('is-active');
+    })
+}
+allTabsButtons.forEach((button, i) => {
+    button.addEventListener('click', (e) => {
+        removeIsActiveClassButtons();
+        button.classList.add('is-active');
+        removeIsActiveClassTabs();
+        allTabs[i].classList.add('is-active');
+    })
+})
+
+const video = document.querySelector('video');
+const poster = document.querySelector('.accelerator__video-poster');
+poster.addEventListener('click', () => {
+    poster.classList.toggle('play');
+    if(poster.classList.contains('play')) {
+        video.play();
+    } else {
+        video.pause();
+    }
+})
+
